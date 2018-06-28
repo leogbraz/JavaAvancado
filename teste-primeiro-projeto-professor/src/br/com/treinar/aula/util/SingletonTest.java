8e2f269a024b244123504eb446bd1987a6641889
Treinar <Treinar@MICRO17>
2018-06-28 18:57:33 -0300

    pequena alteração

--- teste-primeiro-projeto-professor/src/br/com/treinar/aula/util/SingletonTest.java
+++ teste-primeiro-projeto-professor/src/br/com/treinar/aula/util/SingletonTest.java
@@ -8,10 +8,10 @@ public class SingletonTest {
 		Singleton instance2 = Singleton.getInstance();
 		instance2.imprimirNome("Davi Lucas");
 		Singleton instance3 = Singleton.getInstance();
-		instance3.imprimirNome("Davi Lucas");
+		instance3.imprimirNome("Gleidson");
 		
 		//o construtor na classe Singleton eh privado, portanto, nao eh possivel 
-		//instanciar a classe em um ponto fora dele 
+		//instanciar a classe em um ponto fora dele, poe exemplo abaixo não é possivel fazer 
 		//Singleton instanceX = new Singleton();
 				
 	}
